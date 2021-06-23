8d6cf8cd3feb4c8d54ae653c033ee5ccc7671bb3
Leandro2585 <leo.real2585@gmail.com>
2021-06-23 20:49:00 -0300

    Stoped project

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { SvgProps } from 'react-native-svg'
+import { LinearGradient, SvgProps } from 'react-native-svg'
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
 import { Styles } from './style'
+import { theme } from '../../styles/theme'
 
 type Props = RectButtonProps & {
   title: string;
@@ -11,15 +12,24 @@ type Props = RectButtonProps & {
 }
 
 export const Category: React.FC<Props> = ({
-  title, icon: Icon, checked = false
+  title, icon: Icon, checked = false, ...props
 }: Props) => {
+  const { secondary50, secondary70, secondary85, secondary40 } = theme.colors
   return (
-    <RectButton>
-      <View style={[Styles.content, { opacity: checked ? 1 : 0.4}]}>
-        <View style={checked ? Styles.checked : Styles.check} />
-        <Icon width={48} height={48} />
-        <Text style={Styles.title}>{title}</Text>
-      </View>
+    <RectButton {...props}>
+      <LinearGradient
+        style={Styles.container}
+        colors={[secondary50, secondary70]}>
+
+        <LinearGradient 
+          style={[Styles.content, { opacity: checked ? 1 : 0.5}]}
+          colors={[ checked ? secondary85: secondary50 ,secondary40 ]}
+        >
+          <View style={checked ? Styles.checked : Styles.check} />
+          <Icon width={48} height={48} />
+          <Text style={Styles.title}>{title}</Text>
+        </LinearGradient>
+      </LinearGradient>
     </RectButton>
   )
 }
--- src/components/ListDivider/style.ts
+++ src/components/ListDivider/style.ts
@@ -3,10 +3,11 @@ import { theme } from '../../styles/theme'
 
 export const Styles = StyleSheet.create({
   container: {
-    width: '80%',
+    width: '78%',
     height: 1,
     backgroundColor: theme.colors.secondary40,
-    marginVertical: 21,
+    marginVertical: 31,
+    marginTop: 2,
     alignSelf: 'flex-end'
   }
 })
